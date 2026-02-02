508f0652c31e6dc3f46a697736ae8054865181c9
mobiledockdev <vitalii.email.ua@gmail.com>
2026-02-02 00:29:43 +0000

    Enable GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/WordMaster',
+  assetPrefix: '/WordMaster/',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
